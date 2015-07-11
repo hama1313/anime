29c5cb4c1b28183fd376b28b36e62dbfb070ff86
hamanaka ryou <case.by.case.13@gmail.com>
2015-07-11 18:54:01 +0900

    no message

--- dotinstall.php
+++ dotinstall.php
@@ -7,12 +7,16 @@ var_dump($e->getMessage());
 exit;
 }
 
-$sql = "select * from personal_info";
+$stmt=$dbh->prepare("insert into personal_info(id,name,old)values(?,?,?)");
+$stmt->execute(array("N","M","25"));
+
+echo "dane!";
+/*$sql = "select * from personal_info";
 $stmt = $dbh->query($sql);
 foreach ($stmt->fetchAll(PDO::FETCH_ASSOC) as $user){
 	var_dump($user['name']);
 }
 echo $dbh->query("select count(*) from personal_info")->fetchColumn()."records found";
-
+*/
 $dbh=null;
 
