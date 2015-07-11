88d4edee48e9971a91d6734c6cc979c2ebb47977
hamanaka ryou <case.by.case.13@gmail.com>
2015-07-11 18:05:21 +0900

    no message

--- dotinstall.php
+++ dotinstall.php
@@ -7,6 +7,12 @@ var_dump($e->getMessage());
 exit;
 }
 
-echo "success!";
+$sql = "select * from personal_info";
+$stmt = $dbh->query($sql);
+foreach ($stmt->fetchAll(PDO::FETCH_ASSOC) as $user){
+	var_dump($user['name']);
+}
+echo $dbh->query("select count(*) from personal_info")->fetchColumn()."records found";
 
 $dbh=null;
+
