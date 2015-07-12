07f705a32039aa4eef547f5c73df098f63befe8f
hamanaka ryou <case.by.case.13@gmail.com>
2015-07-12 22:27:34 +0900

    no message

---
+++ New_Anime.php
@@ -0,0 +1,18 @@
+<html>
+<html lang="ja">
+	<head>
+		<meta charset="utf8">
+		<title>新作アニメレビュー</title>
+	</head>
+<body>
+<table border="1">
+  <tr>
+    <td>タイトル</td><td><?php echo $_POST["title"] ?></td></tr>
+    <td>放送年</td><td><?php echo $_POST["year"] ?></td></tr>
+    <td>季節</td><td><?php echo $_POST["season"] ?></td></tr>
+    <td>ストーリー</td><td><?php echo $_POST["summary"] ?></td></tr>
+    <td>ストーリー</td><td><?php echo $_POST["pv"] ?></td></tr>
+  </tr>
+</table>
+</body>
+</html>
