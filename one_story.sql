2fe4dc9ead0b41e94271e6bbfe8650345f94960b
hamanaka ryou <case.by.case.13@gmail.com>
2015-07-18 21:45:56 +0900

    リレーション！！

--- one_story.sql
+++ one_story.sql
@@ -3,7 +3,7 @@
 -- http://www.phpmyadmin.net
 --
 -- Host: 127.0.0.1
--- Generation Time: 2015 年 7 朁E12 日 14:29
+-- Generation Time: 2015 年 7 朁E18 日 14:42
 -- サーバのバージョン： 5.6.24
 -- PHP Version: 5.6.8
 
@@ -28,15 +28,23 @@ SET time_zone = "+00:00";
 
 CREATE TABLE IF NOT EXISTS `one_story` (
   `id` int(11) NOT NULL,
+  `anime_id` int(11) NOT NULL,
   `title` varchar(255) COLLATE utf8_unicode_ci NOT NULL,
-  `comment_time` datetime NOT NULL,
+  `comment_time` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP,
   `comment_title` varchar(255) COLLATE utf8_unicode_ci NOT NULL,
   `comment` text COLLATE utf8_unicode_ci NOT NULL,
   `review_total` int(11) NOT NULL,
   `cs` float NOT NULL,
-  `created_at` datetime NOT NULL,
-  `updated_at` datetime NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+  `created_at` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  `updated_at` datetime NOT NULL ON UPDATE CURRENT_TIMESTAMP
+) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+
+--
+-- テーブルのデータのダンプ `one_story`
+--
+
+INSERT INTO `one_story` (`id`, `anime_id`, `title`, `comment_time`, `comment_title`, `comment`, `review_total`, `cs`, `created_at`, `updated_at`) VALUES
+(1, 1, '1', '0000-00-00 00:00:00', '2', '3', 4, 5, '0000-00-00 00:00:00', '2015-07-18 21:35:54');
 
 --
 -- Indexes for dumped tables
@@ -46,7 +54,7 @@ CREATE TABLE IF NOT EXISTS `one_story` (
 -- Indexes for table `one_story`
 --
 ALTER TABLE `one_story`
-  ADD PRIMARY KEY (`id`);
+  ADD PRIMARY KEY (`id`), ADD UNIQUE KEY `id` (`id`), ADD UNIQUE KEY `id_2` (`id`), ADD KEY `anime_id` (`anime_id`), ADD KEY `anime_id_2` (`anime_id`);
 
 --
 -- AUTO_INCREMENT for dumped tables
@@ -56,7 +64,7 @@ ALTER TABLE `one_story`
 -- AUTO_INCREMENT for table `one_story`
 --
 ALTER TABLE `one_story`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=2;
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
