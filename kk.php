24c7b6b325102b759db9887e3d63422e020b4173
hamanaka ryou <case.by.case.13@gmail.com>
2015-08-23 21:08:12 +0900

    ドットインストール　WebページHTML

---
+++ kk.php
@@ -0,0 +1,83 @@
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+  <meta charset="utf8">
+  <title>アニメレビュー</title>
+  <link rel="stylesheet" type="text/css" href="http://yui.yahooapis.com/3.18.1/build/cssreset/cssreset-min.css">
+  <link rel ="stylesheet" href="kk.css">
+</head>
+
+<body>
+ <!-- <h1>最新のアニメ情報</h1>
+
+
+ <h2>今期アニメ一覧</h2>
+ <h3>更新時間　現在</h3>
+ <div class=""></div>-->
+
+ <div id="container">
+  <div id="headder"><h1><img src="dummy1.jpg" height="100" width="600"></h1></div>
+  <div id="menu">menu
+    <ul>
+      <li><a href=""> ホーム</a></li>
+      <li><a href="">製品情報</a></li>
+      <li><a href="">企業情報</a></li>
+    </ul>
+  </div>
+
+  <div id="main">
+    <div id="contents">
+      <h2>今期アニメ一覧</h2>
+      <p>11111111111111111111111111111</p>
+      <p>22222222222222222222222222222</p>
+      <ul class="products">
+        <li>
+          <img src="dummy1.jpg" height="60" width="60">
+          <p>説明。。。。。。。。。。。。。。。。。。。。。。。。</p>
+          
+        </li>
+        <li>
+          <img src="dummy1.jpg" height="60" width="60">
+          <p>説明。。。。。。。。。。。。。。。。。。。。。。。。</p>
+          
+          <li>
+            <img src="dummy1.jpg" height="60" width="60">
+            <p>説明。。。。。。。。。。。。。。。。。。。。。。。。</p>
+            
+          </ul>
+
+          <h2>今期アニメ一覧2</h2>
+          <div class="sample">
+                <span class="badge">New!</span> 
+                <img src="dummy1.jpg" width="60" height="60">
+                <p>補足。補足。補足。補足。</p>
+          </div>
+
+          <p>33333333333333333333333333333</p>
+          <p>44444444444444444444444444444</p>
+        </div><!--contents-->
+
+        <div id="sidebar">
+          <h3>今期アニメ一覧3</h3>
+          <p>33333333333333333333333333333</p>
+          
+
+          <h3>今期アニメ一覧4</h3>
+          <p>44444444444444444444444444444</p>
+          <ul class=s"submenu">
+            <li><a href="">menu1</li>
+            <li><a href="">menu2</li>
+            <li><a href="">menu3</li>
+          </ul>
+        </div><!--sidebar-->
+      </div><!--main-->
+      <div id="footer">Copyright 2015, anime_review</div>
+
+    </div><!--#container-->
+
+
+
+  </body>
+
+
+
